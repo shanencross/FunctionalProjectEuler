d884e65780e59123bfdf77d4c671ad2e207e78b0
Shanen Cross <shanencross@yahoo.com>
2021-07-21 20:57:06 -0700

    Remove comments
    Co-authored-by: Cat Denton <cat.denton.dev@gmail.com>

--- src/js/multiplesXandYtoLimit.js
+++ src/js/multiplesXandYtoLimit.js
@@ -1,20 +1,7 @@
-// const multiplesXandY = (x, y, upperLimit) => {
-//     const fullArray = Array(upperLimit - 1).fill().map((_, i) => i+1);
-
-//     // const filteredArray = fullArray.filter(element => element % x === 0 || element % y === 0)
-//     const sum = fullArray.reduce( (currentValue, element) => {
-//         console.log(currentValue);
-//         if (element % x === 0 || element % y === 0) {
-//             return currentValue + element;
-//         }
-//         return currentValue;
-//     }, 0);
-
-//     return sum;
-// }
+// this is wrong
 const multiplesXandYtoLimit = (limit) => {
-    return function (x) {
-        return function (y) {
+    return function(x) {
+        return function(y) {
             const fullArray = Array(limit - 1).fill().map((_, i) => i + 1);
             const sum = fullArray.reduce((currentValue, element) => {
                 if (element % x === 0 || element % y === 0) {
