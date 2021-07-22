609fe81893f061e61b50c6d454a1d61711a86ffc
Shanen Cross <shanencross@yahoo.com>
2021-07-21 20:56:43 -0700

    Correct the second test expected value
    Co-authored-by: Cat Denton <cat.denton.dev@gmail.com>

--- __tests__/multiplesXandYtoLimit.tests.js
+++ __tests__/multiplesXandYtoLimit.tests.js
@@ -6,7 +6,7 @@ describe ('multiplesXandYtoLimit',() => {
   });
   test('the function should return a function when given an argument for limit', () => {
     const multiplesXandYto253 = multiplesXandYtoLimit(253)
-    expect(multiplesXandYto253(3)(5)).toEqual(15045)
+    expect(multiplesXandYto253(3)(5)).toEqual(17085)
   })
   test('the function should also work if the limit is a multiple of 3', () => {
     expect(multiplesXandYtoLimit(12)(5)(3)).toEqual(33)
