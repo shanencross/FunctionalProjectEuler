43bc15ae74d22b6a33abc3eb154be635bd6024c2
Shanen Cross <shanencross@yahoo.com>
2021-07-21 19:44:58 -0700

    Add UI logic
    Co-authored-by: Cat Denton <cat.denton.dev@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,3 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import multiplesXandYtoLimit from './js/multiplesXandYtoLimit';
+
+$(document).ready(function () {
+    $("#multiplesForm").submit(function (e) {
+        e.preventDefault();
+
+        const x = $("#multipleX").val();
+        const y = $("#multipleY").val();
+        const limit = $("#limit").val();
+        const sum = multiplesXandYtoLimit(limit)(x)(y);
+
+
+        $("#sum").text(sum);
+        $("#results").show();
+
+    });
+});
